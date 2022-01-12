7dcecdc8fd2924b5084d0851ef29365e982fed66
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-01-12 17:28:13 -0500

    Starting Code

---
+++ Exercises/wheres-sausage-dog-new-game-plus/js/SausageDog.js
@@ -0,0 +1,20 @@
+class SausageDog extends Animal {
+  constructor(x, y, image) {
+    super(x, y, image);
+    this.found = false;
+    this.rotationSpeed = 0.25;
+  }
+  update() {
+    super.update();
+
+    if (this.found) {
+      this.angle += this.rotationSpeed;
+    }
+  }
+  mousePressed() {
+    if (this.overlap(mouseX, mouseY)) {
+      this.found = true;
+    }
+  }
+
+}
