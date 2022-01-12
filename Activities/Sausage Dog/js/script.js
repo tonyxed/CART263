0491fb1f13ec3b1caac0f127fc40e18a4e1313be
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-01-12 17:26:45 -0500

    Activity Done

---
+++ Activities/Sausage Dog/js/script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 50;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+  sausageDogImage = loadImage('assets/images/sausage-dog.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  createAnimals();
+  createDog();
+
+  function createAnimals() {
+    //create the animals
+    for (let i = 0; i < NUM_ANIMALS; i++) {
+      let x = random(0, width);
+      let y = random(0, height);
+      let animalImage = random(animalImages);
+      let animal = new Animal(x, y, animalImage);
+      animals.push(animal);
+    }
+  }
+
+  function createDog() {
+    let x = random(0, width);
+    let y = random(0, height);
+    sausageDog = new SausageDog(x, y, sausageDogImage);
+  }
+}
+
+function draw() {
+  background(0);
+  updateAnimals();
+  updateDog();
+
+  function updateAnimals() {
+    for (let i = 0; i < animals.length; i++) {
+      animals[i].update();
+    }
+
+  }
+
+  function updateDog() {
+    sausageDog.update();
+  }
+}
+
+
+function mousePressed() {
+  sausageDog.mousePressed();
+}
