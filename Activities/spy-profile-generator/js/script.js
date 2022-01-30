76480dc81086cc7901ba268625b2210ad9e591e7
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-01-29 20:00:15 -0500

    Activity Finished

--- Activities/spy-profile-generator/js/script.js
+++ Activities/spy-profile-generator/js/script.js
@@ -1,23 +1,65 @@
-
 "use strict";
 
-function setup() {
-  createCanvas(500,500);
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `*REDACTED**`
+};
 
-  if(annyang){
-    let commands = {
-      'hello': function() {
-          alert(`Howdy!`);
-      },
-      'goodbye': function (){
-        alert('CYA!');
-      }
-    };
-    annyang.addCommands(commands);
-    annyang.start();
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
+
+function preload() {
+  instrumentData = loadJSON('https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json');
+  objectData = loadJSON('https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json');
+  tarotData = loadJSON('https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  let data = JSON.parse(localStorage.getItem('data_spy'));
+  if (data !== null) {
+    let password = prompt('Password?');
+    if (password === data.password) {
+      spyProfile.name = data.name;
+      spyProfile.alias = data.alias;
+      spyProfile.secretWeapon = data.secretWeapon;
+      spyProfile.password = data.password;
+    }
+  } else {
+    generateSpyProfile();
   }
 }
 
+function generateSpyProfile() {
+  spyProfile.name = prompt(`What is your name?`);
+  let instrument = random(instrumentData.instruments);
+  spyProfile.alias = `The ${instrument}`;
+  spyProfile.secretWeapon = random(objectData.objects);
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
+
+  localStorage.setItem('data_spy',JSON.stringify(spyProfile));
+}
+
 function draw() {
-background(0);
+  background(255);
+
+  let profile = `**CONFIDENTIAL!**
+
+  Name: ${spyProfile.name}
+  Alias: ${spyProfile.alias}
+  Secret Weapon: ${spyProfile.secretWeapon}
+  Password: ${spyProfile.password}`;
+
+  push();
+  textFont(`Bold, monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
 }
