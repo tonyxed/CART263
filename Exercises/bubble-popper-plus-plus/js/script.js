e52a25e79d443d85cfb8940507a73f9db908bb4f
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-02-08 14:35:59 -0500

    E4: counts the amount of bubbles being popped

--- Exercises/bubble-popper-plus-plus/js/script.js
+++ Exercises/bubble-popper-plus-plus/js/script.js
@@ -10,13 +10,15 @@ let handpose = undefined;
 //current set of predictions
 let predictions = [];
 let bubble = undefined;
+// bubbles popped
+let bubblesPop = 0;
 
 function setup() {
   createCanvas(640, 480);
 
   // access user's webcam
-  video = createCapture(VIDEO);
-  video.hide();
+  //video = createCapture(VIDEO);
+  //video.hide();
 
   //loads handpose model
   handpose = ml5.handpose(video, {
@@ -39,9 +41,19 @@ function setup() {
     vy: -2
   };
 }
+let state = 'loading'; //state
 
 function draw() {
   background(0);
+  if(state === 'loading'){
+    loadingScreen();
+  } else if(state === 'game'){
+      bubblePopText();
+      simulation();
+  }
+
+// bubble and handpose simulation
+function simulation(){
   if (predictions.length > 0) {
     let hand = predictions[0];
     let index = hand.annotations.indexFinger;
@@ -70,11 +82,18 @@ function draw() {
     if (d < bubble.size / 2) {
       bubble.x = random(width);
       bubble.y = height;
+      bubblesPop += 1;
     }
   }
   //bubble move
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
+
+  //speed of the bubble on the y axis
+  let speed = bubble.speed;
+  bubble.speed = random(.5,3.5);
+  bubble.y -= bubble.speed;
+
   if (bubble.y < 0) {
     bubble.x = random(width);
     bubble.y = height;
@@ -83,5 +102,23 @@ function draw() {
   fill(0, 100, 200);
   noStroke();
   ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+}
+}
+// text showing the amount of bubbles popped
+function bubblePopText(){
+  push();
+  fill(255,0,0);
+  textSize(30);
+  text(bubblesPop, width/2, 50);
+  pop();
+}
+// text showing a loading screen
+function loadingScreen(){
+  push();
+  fill(255);
+  textSize(40);
+  textAlign(CENTER,CENTER);
+  text("LOADING CONTENT...");
   pop();
 }
