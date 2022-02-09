0be681dae59419b2d42b2af21807182fe84565a2
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-02-08 21:36:02 -0500

    Project1: Template

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+The Need for the Speed!
+Anthony Calderone
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,10 @@ author, and this description to match your project!
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
 
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
 
 }
