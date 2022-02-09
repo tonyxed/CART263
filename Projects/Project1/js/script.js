419fbd3b5378915fdb072f0b552693e669642b3f
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-02-08 21:56:11 -0500

    Project1: Basics

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -2,16 +2,26 @@
 The Need for the Speed!
 Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+My own silly take on the film; Need For Speed!
 */
-
 "use strict";
 
-function setup() {
+//pictures
+let userPic;
 
+//canvas properties
+const WIDTH = 900;
+const HEIGHT = 1000;
+//preload
+function preload() {
+  userPic = loadImage("assets/images/user.png");
+}
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
+  background(0);
 
 }
