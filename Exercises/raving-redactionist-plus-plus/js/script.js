9e70e79d3fd21e89d3b267e727772829810d7621
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-03-08 18:02:31 -0500

    E6: Started Code

--- Exercises/raving-redactionist-plus-plus/js/script.js
+++ Exercises/raving-redactionist-plus-plus/js/script.js
@@ -1,11 +1,33 @@
 /**
-Title of Project
-Author Name
+Raving Redactionist-Plus-Plus
+Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
 "use strict";
 
-// Code goes here
+//on click function of top-secret spans
+$(`.top-secret`).on(`click`, redact);
+
+//sets the interval to 5 seconds
+setInterval(revelation, 500);
+
+//selects all redacted class's
+function revelation(){
+  $(`.redacted`).each(attemptReveal); //.each is for the array like object, redacted class in the index.html
+}
+
+//attempts to reveal text
+function attemptReveal(){
+let r = Math.random();
+if (r < .1){
+  $(this).removeClass(`redacted`);
+  $(this).addClass(`revealed`);
+}
+  }
+
+//when clicked on the spans
+function redact(event){
+$(this).removeClass(`revealed`);
+$(this).addClass(`redacted`);
+}
