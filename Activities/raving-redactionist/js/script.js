47d8ad4f8770308401082ba4bbe6a4b72053af52
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-03-08 14:44:19 -0500

    Activity Finished

--- Activities/raving-redactionist/js/script.js
+++ Activities/raving-redactionist/js/script.js
@@ -5,3 +5,29 @@ Anthony Calderone
 */
 
 "use strict";
+
+//on click function of top-secret spans
+$(`.top-secret`).on(`click`, redact);
+
+//sets the interval to 5 seconds
+setInterval(revelation, 500);
+
+//selects all redacted class's
+function revelation(){
+  $(`.redacted`).each(attemptReveal); //.each is for the array like object, redacted class in the index.html
+}
+
+//attempts to reveal text
+function attemptReveal(){
+let r = Math.random();
+if (r < .1){
+  $(this).removeClass(`redacted`);
+  $(this).addClass(`revealed`);
+}
+  }
+
+//when clicked on the spans
+function redact(event){
+$(this).removeClass(`revealed`);
+$(this).addClass(`redacted`);
+}
