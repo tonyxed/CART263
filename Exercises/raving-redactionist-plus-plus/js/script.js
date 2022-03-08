187bc9acc6dc79e00f83e0877a814becc3d4a8cd
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-03-08 18:37:33 -0500

    E6: Added button to show how many revealed messages there are

--- Exercises/raving-redactionist-plus-plus/js/script.js
+++ Exercises/raving-redactionist-plus-plus/js/script.js
@@ -8,26 +8,32 @@ Anthony Calderone
 
 //on click function of top-secret spans
 $(`.top-secret`).on(`mouseover`, redact);
+$("button").on(`click`, buttonClick);
 
 //sets the interval to 5 seconds
-setInterval(revelation, 500);
+setInterval(revelation, 1000);
 
-//selects all redacted class's
-function revelation(){
+//selects all redacted
+function revelation() {
   $(`.redacted`).each(attemptReveal); //.each is for the array like object, redacted class in the index.html
 }
 
 //attempts to reveal text
-function attemptReveal(){
-let r = Math.random();
-if (r < .1){
-  $(this).removeClass(`redacted`);
-  $(this).addClass(`revealed`);
-}
+function attemptReveal() {
+  let r = Math.random();
+  if (r < .1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
   }
+}
 
 //when clicked on the spans
-function redact(event){
-$(this).removeClass(`revealed`);
-$(this).addClass(`redacted`);
+function redact(event) {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+//click the button and an alert pops up
+function buttonClick(){
+  alert($(`.revealed`).length);
 }
