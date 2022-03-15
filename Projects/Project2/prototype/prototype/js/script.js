ceed01646e898e07553f611bddeb23b0baa2f002
Anthony Calderone <anthony.calderone2000@yahoo.com>
2022-03-15 11:55:31 -0400

    P2: Prototype started coding

---
+++ Projects/Project2/prototype/prototype/js/script.js
@@ -0,0 +1,17 @@
+/**
+Racer; start!
+Anthony Calderone
+
+Prototype: Use your exceptional typing capibilities to race fellow users(AI).
+Your character's speed depends on how fast you can type, and with minimal mistakes. The idea comes from the website; https://play.typeracer.com/
+I felt that I needed to create something that can utilise a junction of what was introduced this semester, therefore, Racer; start was born.
+This project will encoorporate JQUERY, JSON, CSS, and obviously JAVASCRIPT.
+
+-correct word/letters turn green on correct and vice versa with red on wrong
+-players move on correct spelling/what kind of movement???
+-AI???
+-prompt showing up asking which level to type on? easy to impossible with harder sentences + less time to type
+-accuracy to the typing? WPM??? CPM???
+*/
+
+"use strict";
